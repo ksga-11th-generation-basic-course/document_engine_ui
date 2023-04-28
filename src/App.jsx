02f3ec4026cdd378956ce0f4f40d026b71bd7e73
Sovannak Kheng <sovannak.khengg@gmail.com>
2023-04-28 11:10:11 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-2xl font-bold">Do your best everyone.</h1>
+    </div>
+  );
+}
+
+export default App;
