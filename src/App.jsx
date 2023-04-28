59f59e92b7a8941d61af5379a11f3663bda3c36d
Sovannak Kheng <sovannak.khengg@gmail.com>
2023-04-28 15:44:12 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import NavigationBar from "./components/NavigationBar";
+
 function App() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-2xl font-bold">Do your best everyone.</h1>
+      {/* <h1 className="text-2xl font-bold">Do your best everyone.</h1> */}
+      <NavigationBar/>
     </div>
   );
 }
