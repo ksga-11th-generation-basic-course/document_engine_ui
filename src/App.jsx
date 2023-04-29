13ce2d84155aba04df84991047f45a8a13fecd07
Sovannak Kheng <sovannak.khengg@gmail.com>
2023-04-29 16:42:02 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-2xl font-bold">Do your best everyone.</h1>
-    </div>
+    <div className="font-ssp"></div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ssp: ["Source Sans Pro", "sans-serif"],
+      },
+      colors: {
+        primary : '#1E9CEF',
+        secondary : '#F4F4F4',
+        accent : '#526581',
+        'bg-primary' : '#EDF9FF',
+        'text-primary' : '#37352F'
+      },
+      fontSize: {
+        '12px' : '12px',
+        '15px' : '15px',
+        '16px' : '16px',
+        '18px' : '18px',
+        '20px' : '20px',
+        '22px' : '22px',
+        '24px' : '24px',
+        '36px' : '36px',
+        '48px' : '48px',
+      }
+    },
   },
   plugins: [],
-}
+};
