297ffb31ce04b235db7b4e673f097f5a21e57a46
SeangNgorn Khy <seangngornkhy@SeangNgorns-MacBook-Pro.local>
2023-05-01 18:33:04 +0700

    neww

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
+import Form_Sucess from "./pages/Form_Sucess";
 import Login from "./pages/Login";
 import LogOut from "./pages/LogOut";
 function App() {
   return (
     <div className="font-ssp">
       {/* <Login/> */}
-      <LogOut></LogOut>
+      {/* <LogOut></LogOut> */}
+      <Form_Sucess></Form_Sucess>
     </div>
   );
 }
