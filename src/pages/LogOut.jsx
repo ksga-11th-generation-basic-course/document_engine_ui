b3ccaf7221aabec35aa72cf65a652f8969098e11
SeangNgorn Khy <seangngornkhy@SeangNgorns-MacBook-Pro.local>
2023-05-01 16:42:26 +0700

    new2

--- src/pages/LogOut.jsx
+++ src/pages/LogOut.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Logo from "../assets/images/Logo.svg";
-import RightImage from "../assets/images/Login/Right-img-login.svg";
-import LeftImage from "../assets/images/Login/Left-img-login.svg";
+import Right1Image from "../assets/images/Login/Right-img-logout.svg";
+import Left1Image from "../assets/images/Login/Left-img-logout.svg";
 import Google from "../assets/images/Login/Google.svg";
 import Facebook from "../assets/images/Login/Facebook.svg";
 export default function () {
@@ -9,8 +9,8 @@ export default function () {
     <div className="flex justify-center items-center bg-[#EDF9FF] text-[#37352F]">
       <div className="flex justify-center items-center min-h-screen relative overflow-hidden">
         <img src={Logo} className="absolute top-5 left-0" />
-        <img src={LeftImage} className="mr-2" />
-        <div className="bg-white p-12 flex flex-col justify-center gap-y-6 rounded-3xl shadow-md">
+        <img src={Left1Image} className="mr-2" />
+        <div className="bg-white p-10 flex flex-col justify-center gap-y-6 rounded-3xl shadow-md">
          <div class=" xs:p-0 mx-auto md:w-full md:max-w-md">
                     <h1 class="font-bold text-center text-blue-500  text-2xl">Sign Up</h1> 
                     <div class="px-5 py-7">
@@ -22,11 +22,6 @@ export default function () {
                         <input type="text" class="border border-blue-400 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"  placeholder='Password' />
                         <label class="font-semibold text-sm text-gray-600 pb-3block">Confirm Password</label>
                         <input type="text" class="border border-blue-400 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"  placeholder='Confirm Password'/>
-                        {/* <div className='p-2 mb-3'>
-                            <div class="text-left">
-                                <a href="#" class="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700">Forgot Password?</a>
-                            </div>
-                        </div>   */}
                         <button type="button" class="transition pb-5 duration-200  w-full py-2.5 rounded-lg text-sm  font-semibold text-center inline-block">
                             Do you have an account?<a href="" className='text-blue-500 pr-2 '> Sign Up</a>
                         </button> 
@@ -43,10 +38,10 @@ export default function () {
                             </p>
                         </div>
                     </div>   
-                    <div class="p-3 ">
-                      <div class="grid grid-cols-2 gap-6 ">
-                          <button            class="flex items-center text-center py-2 px-2 text-sm uppercase rounded bg-white border border-gray-300 hover:border-transparent hover:text-gray-700  font-bold transition transform hover:-translate-y-0.5"          >
-                              <svg              xmlns="http://www.w3.org/2000/svg"              class="w-6 h-6 mr-5"              viewBox="0 0 48 48"            >
+                    <div class="p-2">
+                      <div class="grid grid-cols-2 gap-6">
+                          <button            class="flex justify-center items-center  py-2 px-4 text-sm uppercase rounded bg-white border border-gray-300 hover:border-transparent hover:text-gray-700  font-bold transition transform hover:-translate-y-0.5"          >
+                              <svg              xmlns="http://www.w3.org/2000/svg"              class="w-6 h-6 mr-4"              viewBox="0 0 48 48"            >
                                   <path                fill="#fbc02d"                d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4 12.955 4 4 12.955 4 24s8.955 20 20 20 20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z"              />
                                   <path                fill="#e53935"                d="m6.306 14.691 6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4 16.318 4 9.656 8.337 6.306 14.691z"              />
                                   <path                fill="#4caf50"                d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.91 11.91 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44z"              />
@@ -54,7 +49,7 @@ export default function () {
                               </svg>
                               Google
                           </button>
-                          <button            class="flex items-center py-2 px-4 font-bold text-sm uppercase rounded border border-gray-300    hover:border-transparent hover:text-gray-700   transition transform hover:-translate-y-0.5"          >
+                          <button            class="flex justify-center items-center py-2 px-4 font-bold text-sm uppercase rounded border border-gray-300    hover:border-transparent hover:text-gray-700   transition transform hover:-translate-y-0.5"          >
                               <svg
                                   class="w-6 h-6 mr-4 text-blue-600 fill-current"
                                   xmlns="http://www.w3.org/2000/svg"
@@ -74,7 +69,7 @@ export default function () {
 
                         
         </div>
-        <img src={RightImage} />
+        <img src={Right1Image} />
     </div>
   )
 }
