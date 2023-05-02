9ba51f9367effdf0b18a91ffc9f06ab02afcfb5d
SeangNgorn Khy <seangngornkhy@SeangNgorns-MacBook-Pro.local>
2023-05-02 11:49:27 +0700

    new3

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import Form_Sucess from "./pages/Form_Sucess";
 import Login from "./pages/Login";
 import LogOut from "./pages/LogOut";
+import VerifyAddress from "./pages/VerifyAddress";
 function App() {
   return (
     <div className="font-ssp">
+      <VerifyAddress></VerifyAddress>
       {/* <Login/> */}
       {/* <LogOut></LogOut> */}
-      <Form_Sucess></Form_Sucess>
+      {/* <Form_Sucess></Form_Sucess> */}
     </div>
   );
 }
