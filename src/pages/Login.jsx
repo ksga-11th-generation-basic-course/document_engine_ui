21592ccf5b02bb1a0ea87ae8c0d4421f9f7d5ac0
SeangNgorn Khy <seangngornkhy@SeangNgorns-MacBook-Pro.local>
2023-05-09 16:23:37 +0700

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,23 +13,18 @@ const Login = () => {
         <div className="bg-white p-10 flex flex-col justify-center gap-y-6 rounded-3xl shadow-md">
          <div class=" xs:p-0 mx-auto md:w-full md:max-w-md">
                     <h1 class="font-bold text-center text-blue-500  text-2xl">Sign in</h1> 
-                    <div class="px-5 py-7">
-                         {/* <label class="font-semibold text-sm text-gray-600 pb-3 block">Username</label> */}
-                        {/* <input type="text" class="border border-blue-400 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"  placeholder='Username'/> */}
+                    <div class="px-5 py-5">
                         <label class="font-semibold text-sm text-gray-600 pb-3 block">E-mail</label>
                         <input type="email" class="border border-blue-400 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"  placeholder='example@gmail.com'/>
                         <label class="font-semibold text-sm text-gray-600 pb-3 block">Password</label>
                         <input type="text" class="border border-blue-400 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"  placeholder='Password' />
-                        {/* <label class="font-semibold text-sm text-gray-600 pb-3block">Confirm Password</label> */}
-                        {/* <input type="text" class="border border-blue-400 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"  placeholder='Confirm Password'/> */}
                         <button type="button" class="transition pb-5 duration-200  w-full py-2.5  text-sm  font-semibold text-left inline-block">
                             Forgot your password?
                         </button> 
-                        {/* <p></p> */}
                         <button type="button" class="transition  duration-200 bg-primary hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
                             Sign in
                         </button>
-                        <button type="button" class="transition  duration-200 text-primary -4 pt-10    w-full py-2.5  text-sm  font-semibold text-center inline-block">
+                        <button type="button" class="transition  duration-200 text-primary pt-5 w-full py-2.5  text-sm  font-semibold text-center inline-block">
                             Sign in
                         </button>
                     </div>
@@ -44,7 +39,7 @@ const Login = () => {
                     </div>   
                     <div class="p-2">
                       <div class="grid grid-cols-2 gap-6">
-                          <button            class="flex justify-center items-center  py-2 px-4 text-sm uppercase rounded bg-white border border-gray-300 hover:border-transparent hover:text-gray-700  font-bold transition transform hover:-translate-y-0.5"          >
+                          <button            class="flex justify-center items-center  py-2 px-4 text-sm uppercase rounded bg-white border border-gray-300  hover:border-transparent hover:text-gray-700  font-bold transition transform hover:-translate-y-0.5"          >
                               <svg              xmlns="http://www.w3.org/2000/svg"              class="w-6 h-6 mr-4"              viewBox="0 0 48 48"            >
                                   <path                fill="#fbc02d"                d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4 12.955 4 4 12.955 4 24s8.955 20 20 20 20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z"              />
                                   <path                fill="#e53935"                d="m6.306 14.691 6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4 16.318 4 9.656 8.337 6.306 14.691z"              />
@@ -53,7 +48,7 @@ const Login = () => {
                               </svg>
                               Google
                           </button>
-                          <button            class="flex justify-center items-center py-2 px-4 font-bold text-sm uppercase rounded border border-gray-300    hover:border-transparent hover:text-gray-700   transition transform hover:-translate-y-0.5"          >
+                          <button            class="flex justify-center items-center py-2 px-4 font-bold text-sm uppercase rounded border border-gray-300 hover:border-black   hover:border-transparent hover:text-gray-700   transition transform hover:-translate-y-0.5"          >
                               <svg
                                   class="w-6 h-6 mr-4 text-blue-600 fill-current"
                                   xmlns="http://www.w3.org/2000/svg"
