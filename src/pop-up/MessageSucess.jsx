594b27a0c8a35d0c0f9273e4ac67aa5089e0b0b6
Sovannak Kheng <sovannak.khengg@gmail.com>
2023-05-26 00:50:38 +0700

    init

--- src/pop-up/MessageSucess.jsx
+++ src/pop-up/MessageSucess.jsx
@@ -1,24 +1,57 @@
-import React from 'react'
-import Message from "../assets/images/popUp/messageSucess.svg"
-import Cancel from "../assets/images/popUp/cancel.png"
-import "./font.css"
+import React from "react";
+import Message from "../assets/images/popUp/messageSucess.svg";
+import CloseIcon from "../assets/reset_pw_popup/closeIcon.svg";
 export default function MessageSucess() {
   return (
-    <div className='flex justify-center items-center bg-slate-400 w-full h-[100vh]'>
-        <div className="popup w-[641px] h-[381px] relative flex  justify-center items-center rounded-md shadow-sm bg-white ">
-            <div className='img  absolute top-5 right-[24px]'>
-                <img src={Cancel} alt="" />
-            </div>
-            <div className='icon  flex  flex-col justify-center items-center r'>
-                <img src={Message} alt=""  className='w-[93pxx] pb-10'/>
-                <div className='text text-accent text-center whitespace-nowrap leading-7 text-xl' >
-                    <p>Thank you for reaching out to us! We have </p>
-                    <p>received your message and will get back to</p>
-                    <p> you as soon as possible. </p>
+    <div>
+      <div
+        className="relative z-10"
+        aria-labelledby="modal-title"
+        role="dialog"
+        aria-modal="true"
+      >
+        <div className="fixed inset-0 bg-gray-200 bg-opacity-70 transition-opacity"></div>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className=" relative h-[380px] transform overflow-hidden rounded-lg bg-white text-left lg:shadow-xl transition-all sm:my-12 sm:w-full sm:max-w-[540px] ">
+              <div
+                className="flex justify-end pt-4 pr-4 cursor-pointer"
+                onClick={() => setOpen(false)}
+              >
+                <img src={CloseIcon} alt="" className="" />
+              </div>
+              <div className="flex flex-col justify-center items-center px-4 ">
+                <img src={Message} alt="" className="md:w-24" />
+              </div>
+              <form className="bg-white px-4 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex justify-center">
+                  <div className="mt-2 text-center  sm:mt-0 sm:text-left">
+                    <h3
+                      className=" text-baseleading-12 text-20px text-accent flex flex-col justify-center font-normal"
+                      id="modal-title"
+                    >
+                      <p className="text-center">Thank you for reaching out to us! We have</p>
+                      <p className="text-center">received your message and will get back to</p>
+                      <p className="text-center">you as soon as possible. </p>
+                    </h3>
+                  </div>
                 </div>
-                <button className='w-[150px] h-[40px] text-white mt-10 bg-primary rounded-lg'>Thank You</button>
+              </form>
+
+              <div className="mt-4 ml-2 flex justify-center">
+                <button
+                  type="button"
+                  class="text-white bg-[#1E9CEF] hover:bg-[#1E9CEF]/90 focus:outline-none  font-bold rounded-lg text-16px px-12 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 mr-2 mb-10"
+                >
+                  Thank You
+                </button>
+              </div>
+              {/* flex-row-reverse rounded-md bg-[#1E9CEF] px-36 py-3 text-sm font-semibold text-white  hover:bg-blue-500 sm:ml-3 sm:w-auto */}
             </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
