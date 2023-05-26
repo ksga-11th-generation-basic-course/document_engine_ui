abd202d93f752bfa9391131069ced9f279371f66
Sorn-Sreynorng <sorn.sreynorng2020@gmail.com>
2023-05-26 01:37:20 -0700

    workspace collaborators

--- src/pop-up/WorkspaceCallaboratorMember.jsx
+++ src/pop-up/WorkspaceCallaboratorMember.jsx
@@ -47,7 +47,10 @@ function WorkspaceCallaboratorMember() {
 
                                         <div className=' pl-6 pb-12'>
                                             <div className='flex mb-6'>
-                                                <div className='w-10 h-10 bg-[#37352F] rounded-3xl'></div>
+                                                <div className='w-10 h-10 pt-2 bg-[#37352F] rounded-3xl'>
+
+                                                <label className='ml-4 text-[#FFFFFF] font-semibold'>S</label>
+                                                </div>
                                                 <label className='mt-2 ml-4 text-[#526581]'>Sorn Sreynorng</label>
 
                                                 <div className='flex justify-center item-center mt-2  mb-2 ml-8 bg-[#1E9CEF] w-20 rounded-lg bg-opacity-10'>
@@ -55,12 +58,18 @@ function WorkspaceCallaboratorMember() {
                                                 </div>
                                             </div>
                                             <div className='flex mb-6' >
-                                                <div className='w-10 h-10 bg-[#F28F8F] rounded-3xl'></div>
+
+                                                <div className='w-10 h-10 pt-2 bg-[#F28F8F] rounded-3xl'>
+                                                    <label className='ml-4 text-[#FFFFFF] font-semibold'>R</label>
+                                                </div>
                                                 <label className='mt-2 ml-4 text-[#526581]' >Ruos Raksa</label>
                                                 <label className='mt-2 ml-2 text-[#526581]' >(You)</label>
                                             </div>
                                             <div className='flex'>
-                                                <div className='w-10 h-10 bg-[#1877F2] rounded-3xl'></div>
+                                                <div className='w-10 h-10 pt-2 bg-[#1877F2] rounded-3xl'>
+
+                                                <label className='ml-4 text-[#FFFFFF] font-semibold'>C</label>
+                                                </div>
                                                 <label className='mt-2 ml-4 text-[#526581]'>Chhum Lyheng</label>
                                             </div>
                                         </div>
