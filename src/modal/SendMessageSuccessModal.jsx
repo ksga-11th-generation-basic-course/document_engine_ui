13ec1a41dbc8ae600b6a770dcf1ad7febdef7cde
raksa417 <ruosraksa417@gmail.com>
2023-06-12 11:37:28 +0700

    msg

--- src/modal/SendMessageSuccessModal.jsx
+++ src/modal/SendMessageSuccessModal.jsx
@@ -1,54 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import { Modal } from "react-daisyui";
+import close from "../assets/dashboard_image/close.svg";
+import deleteprofile from "../assets/dashboard_image/deleteprofile.svg";
 import sendMessageSuccess from '../assets/reset_pw_popup/sendMessageSuccess.svg'
 import CloseIcon from '../assets/reset_pw_popup/closeIcon.svg'
 
-function SendMessageSuccessModal() {
+export const SendMessageSuccessModal = ({
+  openSendMessageSuccess,
+  setOpenSendMessageSuccess,
+}) => {
   return (
-    <div>
-
-            <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-
-                <div className="fixed inset-0 bg-gray-200 bg-opacity-70 transition-opacity"></div>
-
-
-
-                <div className="fixed inset-0 z-10 overflow-y-auto">
-
-                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-
-                        <div className=" relative transform overflow-hidden rounded-lg bg-white text-left lg:shadow-xl transition-all sm:my-12 sm:w-full sm:max-w-lg ">
-                            <div className='flex justify-end pt-4  pr-4'>
-                                <img src={CloseIcon} alt="" className='' />
-                            </div>
-                            <div className='flex flex-col justify-center items-center px-4 '>
-                                <img src={sendMessageSuccess} alt="" className='md:w-16' />
-                            </div>
-                            <form className="bg-white px-4  sm:p-6 sm:pb-4  ">
-                                <div className="sm:flex justify-center">
-                                    <div className=" text-center  sm:mt-0 sm:text-left">
-                                        <h3 className=" text-baseleading-12 text-[#6B7280] flex justify-center font-normal " id="modal-title">Thank you for reaching out to us!</h3>
-                                        <h3 className=" text-baseleading-12 text-[#6B7280] flex justify-center font-normal" id="modal-title">  We have received your message and will</h3>
-                                        <h3 className=" text-baseleading-12 text-[#6B7280] flex justify-center font-normal  mb-2" id="modal-title">get back to you as soon as possible.</h3>
-
-                                    </div>
-                                </div>
-                            </form>
-
-                            <div className='mt-4 ml-2 flex justify-center'>
-                              
-                                <button type="button" class="text-white bg-primary hover:bg-primary/90 focus:outline-none  font-semibold  rounded-lg text-sm px-9 py-2.5 text-center inline-flex items-center mr-2 mb-10">
-                                    Thank You
-                                </button></div>
-                            {/* flex-row-reverse rounded-md bg-[#1E9CEF] px-36 py-3 text-sm font-semibold text-white  hover:bg-blue-500 sm:ml-3 sm:w-auto */}
-                        </div>
-                    </div>
-                </div>
+    <div className="w-full">
+      <Modal open={openSendMessageSuccess} onClickBackdrop={() => setOpenSendMessageSuccess(!openSendMessageSuccess)}>
+        <div className="w-[540px] bg-white rounded-lg p-3 md:w-[300px]">
+          <div className="flex justify-end">
+            <button
+              type="button"
+              onClick={() => setOpenSendMessageSuccess(!openSendMessageSuccess)}
+            >
+              <img src={close} className="md:w-6"/>
+            </button>
+          </div>
+          <div className="px-14 space-y-5 text-accent md:px-5">
+            <div className="flex justify-center items-center">
+              <img src={sendMessageSuccess} className="md:w-16"/>
             </div>
+            <p className="font-normal text-accent text-22px leading-7 py-3 text-center md:text-16px">
+                        Thank you for reaching out to us! We have received your message and will get back to you as soon as possible. 
+            </p>
 
-
+            <div className="flex justify-center items-center gap-5 text-18px font-semibold pb-5 md:text-14px">
+              <button className="bg-primary text-white  px-10 py-3 rounded-lg md:px-5 md:py-2.5" onClick={() => setOpenSendMessageSuccess(!openSendMessageSuccess)}>
+                      Thank you
+              </button>
+            </div>
+          </div>
         </div>
-  )
-}
-
-export default SendMessageSuccessModal
+      </Modal>
+    </div>
+  );
+};
--- src/pages/ContactUS.jsx
+++ src/pages/ContactUS.jsx
@@ -5,10 +5,10 @@ import IG from "../assets/images/Contact/IG.svg";
 import Face from "../assets/images/Contact/Facebook.svg";
 import Tele from "../assets/images/Contact/Telegram .svg";
 import { NavBar } from "../components/NavBar";
-import SendMessageSuccessModal from "../modal/SendMessageSuccessModal";
+import { SendMessageSuccessModal } from "../modal/SendMessageSuccessModal";
 
 export const ContactUs = () => {
-  const [visible, setVisble] = useState(false);
+  const [openSendMessageSuccess, setOpenSendMessageSuccess] = useState(false);
   
   return (
     <div className="min-h-screen bg-white">
@@ -26,15 +26,13 @@ export const ContactUs = () => {
             <h2 className="text-4xl font-bold pb-5 text-accent">
               Let's <span className="text-primary">Talk!</span>
             </h2>
-            <div className="text-justify text-12px xl:text-10px lg:text-12px   md:text-16px md:whitespace-nowrap sm:pr-16 sm:text-12px text-xl text-accent font-semibold">
-              <p>To request a quote or what to meet up for coffee, </p>
-              <p>
-                contact us directly or fill out the form we will get back to
-              </p>
-              <p>your promptly </p>
+            <div className="text-justify text-12px xl:text-10px lg:text-16px lg:mt-0 lg:mb-8 lg:w-72 md:text-14px md:leading-6 text-xl text-accent font-semibold">
+                    To request a quote or what to meet up for coffee, 
+                      contact us directly or fill out the form we will get back to
+                    your promptly 
             </div>
           </div>
-          <div className="form relative  top-10 lg:top-2">
+          <div className="form relative  top-10 lg:top-2 md:top-0">
             <label
               htmlFor=""
               className="text-xl sm:text-14px text-[#575E69] font-semibold"
@@ -57,9 +55,10 @@ export const ContactUs = () => {
                                 resize
                                 text-accent
                                 font-semibold
+                                md:py-2
                                 "
             />
-            <div className="flex flex-row pt-10">
+            <div className="flex flex-row pt-10 lg:pt-5">
               <div className="email text-[#575E69] mb-2">
                 <label className="text-xl  sm:text-14px font-semibold">
                   Your Message
@@ -113,8 +112,11 @@ export const ContactUs = () => {
                             font-semibold
                             rounded-lg
                             hover:bg-primary/90
+                            md:text-14px
+                            md:py-1.5
+                            md:px-9
                             "
-                    onClick={() => setVisble(!visible)}
+                    onClick={() => setOpenSendMessageSuccess(!openSendMessageSuccess)}
             >
               Send
             </button>
@@ -127,31 +129,31 @@ export const ContactUs = () => {
           <img
             src={Contact}
             alt=""
-            className=" md:w-[560px] md:px-8  sm:px-10 mt-28"
+            className="md:hidden"
           />
           <div className="">
-            <div className="flex mt-12 sm:flex justify-center items-center">
+            <div className="flex mt-12 sm:flex justify-center items-center md:ml-10">
               <img src={Message} alt="" className="w-7 mr-2 " />
-              <a href="#" className="pr-6 sm:pr-8 text-accent_secondary text-20px">
+              <a href="#" className="pr-6 sm:pr-8 text-accent_secondary text-20px md:text-16px">
                 docengine@gmail.com
               </a>
             </div>
-            <div className="Image flex flex-row sm:justify-center sm:w-58 sm:ml-3 top-12 md:top-10 w-56  mt-6 md:pb-10">
-              <div className="img1 pr-12 sm:pr-14 ">
-                <img src={Face} alt="" className="" />
+            <div className="Image flex flex-row space-x-5 sm:justify-center sm:w-58 sm:ml-5 top-12 w-56  mt-6 md:pb-10">
+              <div className="img1">
+                <img src={Face} alt="" className="md:w-8" />
               </div>
-              <div className="img2 pr-12  sm:pr-14">
-                <img src={Tele} alt="" />
+              <div className="img2">
+                <img src={Tele} alt="" className="md:w-8"/>
               </div>
               <div className="img3">
-                <img src={IG} alt="" />
+                <img src={IG} alt="" className="md:w-8"/>
               </div>
             </div>
           </div>
         </div>
       </div>
 
-          {visible ? <SendMessageSuccessModal/> : null}
+        {openSendMessageSuccess ? <SendMessageSuccessModal openSendMessageSuccess={openSendMessageSuccess} setOpenSendMessageSuccess={setOpenSendMessageSuccess} /> : null}
     </div>
   );
 };
