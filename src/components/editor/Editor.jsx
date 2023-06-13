a15c330b944a1502bbefd9e9c5abf51eee86f2b8
Sovannak Kheng <sovannak.khengg@gmail.com>
2023-06-13 19:43:17 +0700

    init

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -4,75 +4,125 @@ import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import styles from "../../App.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { createBlockSuccess } from "../../redux/slice/blockSlice/blockSlice";
-import { createBlock, getBlockBydoucmentId } from "../../redux/service/blockService/blockService";
+import {
+  createBlock,
+  getBlockBydoucmentId,
+} from "../../redux/service/blockService/blockService";
 import { useParams } from "react-router-dom";
 import { getDocumentByDocumentId } from "../../redux/service/documentService/documentService";
 import { handler } from "daisyui";
 
 export const Editor = () => {
+  // const initialContent = [
+  //   {
+  //     id: "d16e6bf9-7bdf-4225-8c6e-8a5470b85481",
+  //     type: "paragraph",
+  //     props: {
+  //       textColor: "default",
+  //       backgroundColor: "default",
+  //       textAlignment: "left",
+  //     },
+  //     content: [
+  //       {
+  //         type: "text",
+  //         text: "hi",
+  //         styles: {},
+  //       },
+  //     ],
+  //     children: [],
+  //   },
+  //   {
+  //     id: "d16e6bf9-7bdf-4225-8c6e-8a5470b85481",
+  //     type: "paragraph",
+  //     props: {
+  //       textColor: "default",
+  //       backgroundColor: "default",
+  //       textAlignment: "left",
+  //     },
+  //     content: [
+  //       {
+  //         type: "text",
+  //         text: "Hello",
+  //         styles: {},
+  //       },
+  //     ],
+  //     children: [],
+  //   },
+  // ];
+
   const dispatch = useDispatch();
   const param = useParams();
   const documentId = param.id;
   const [blocks, setBlock] = useState([]);
   const block = [...blocks];
 
-  const blockData = useSelector((state) => state.block.blocks)
+  const blockData = useSelector((state) => state.block.blocks);
   useEffect(() => {
     dispatch(getBlockBydoucmentId(documentId));
-  }, [])
+  }, []);
   // console.log(blockData);
 
   // Store data to localstorage
   const data = block;
-  localStorage.setItem('mydata', JSON.stringify(data));
+  localStorage.setItem("mydata", JSON.stringify(data));
 
   // Get data from localstorage
-  const retrievedData = localStorage.getItem('mydata');
+  const retrievedData = localStorage.getItem("mydata");
   const parsedData = JSON.parse(retrievedData);
 
   // Create Block
   const handleCreateBlock = async () => {
     for (let index = 0; index < block.length; index++) {
-      const text = block[index]
-      const types = blocks.filter(obj => obj.type).map(obj => obj.type);
-      const type = types[index]
+      const text = block[index];
+      const types = blocks.filter((obj) => obj.type).map((obj) => obj.type);
+      const type = types[index];
       const response = await createBlock(type, text, documentId);
       const success = dispatch(createBlockSuccess(response));
     }
-  }
+  };
 
   function handleInsert() {
     if (editor) {
       editor.insertBlocks([
-        { type: 'paragraph', text: 'New paragraph' },
-        { type: 'heading', level: 3, text: 'New heading' },
+        { type: "paragraph", text: "New paragraph" },
+        { type: "heading", level: 3, text: "New heading" },
       ]);
     }
   }
   const blockToInsert = {
-    type: 'paragraph', // Example property
-    data: { // Example property
-      text: 'This is some sample text',
-      font: 'Arial',
-      fontSize: 14
-    }
+    type: "paragraph", // Example property
+    data: {
+      // Example property
+      text: "This is some sample text",
+      font: "Arial",
+      fontSize: 14,
+    },
   };
 
   //Editor
   const editor = useBlockNote({
+    // initialContent: initialContent,
     onEditorContentChange: (editor) => {
       // console.log(editor.insertBlocks(blockToInsert,k,"after"));
       // console.log(editor.topLevelBlocks);
       // console.log(editor);
       const content = [];
       setBlock(content);
-      for (let indexOfTopLevelBlocks = 0; indexOfTopLevelBlocks < editor.topLevelBlocks.length; indexOfTopLevelBlocks++) {
+      for (
+        let indexOfTopLevelBlocks = 0;
+        indexOfTopLevelBlocks < editor.topLevelBlocks.length;
+        indexOfTopLevelBlocks++
+      ) {
         const element = editor.topLevelBlocks[indexOfTopLevelBlocks];
-        for (let indexOfContent = 0; indexOfContent < element.content.length; indexOfContent++) {
-          const type = element.type
+        for (
+          let indexOfContent = 0;
+          indexOfContent < element.content.length;
+          indexOfContent++
+        ) {
+          const type = element.type;
           const text = element.content[0].text;
           const level = element.props.level;
-          if (type == 'heading') {
+          if (type == "heading") {
             const dataOfContent = { type: type, text: text, level: level };
             content.push(dataOfContent);
           } else {
@@ -88,14 +138,13 @@ export const Editor = () => {
     },
     theme: "light",
   });
-  
 
   return (
     <div>
-      <input type="submit" value='click' onClick={handleCreateBlock} /><br/>
-      <input type="submit" value='get' onClick={handleInsert} />
-      <BlockNoteView editor={editor} type={'heading'} text={'helo'}/>
+      <input type="submit" value="click" onClick={handleCreateBlock} />
+      <br />
+      <input type="submit" value="get" onClick={handleInsert} />
+      <BlockNoteView editor={editor} type={"heading"} text={"helo"} />
     </div>
-
-  )
+  );
 };
