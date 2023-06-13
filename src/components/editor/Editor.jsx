8dedce07aeeb46f0cd2b13e825abe240af885b25
Sovannak Kheng <sovannak.khengg@gmail.com>
2023-06-13 19:03:14 +0700

    init

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -3,8 +3,46 @@ import "@blocknote/core/style.css";
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import styles from "../../App.module.css";
 
+const initialContent = [
+  {
+    id: "d16e6bf9-7bdf-4225-8c6e-8a5470b85481",
+    type: "paragraph",
+    props: {
+      textColor: "default",
+      backgroundColor: "default",
+      textAlignment: "left",
+    },
+    content: [
+      {
+        type: "text",
+        text: "hi",
+        styles: {},
+      },
+    ],
+    children: [],
+  },
+  {
+    id: "d16e6bf9-7bdf-4225-8c6e-8a5470b85481",
+    type: "paragraph",
+    props: {
+      textColor: "default",
+      backgroundColor: "default",
+      textAlignment: "left",
+    },
+    content: [
+      {
+        type: "text",
+        text: "Hello",
+        styles: {},
+      },
+    ],
+    children: [],
+  },
+];
+
 export const Editor = () => {
   const editor = useBlockNote({
+    initialContent: initialContent,
     onEditorContentChange: (editor) => {
       console.log(editor.topLevelBlocks);
     },
