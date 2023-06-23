7e4a6bee07be941fb117f39c8bc75f11a7c14063
Sovannak Kheng <sovannak.khengg@gmail.com>
2023-06-24 05:21:02 +0700

    init

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1/",
+  baseURL: "http://3.24.123.174:8080/api/v1/",
 });
 
 const token = localStorage.getItem("token");
