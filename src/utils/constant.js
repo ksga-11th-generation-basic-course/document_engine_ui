376d2e2d708cef38eb101c33f4f33b1021b7e9ae
Tithouddomm <ouddomtith2001@gmail.com>
2023-06-27 11:34:20 +0700

    init

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://3.24.123.174:8080/api/v1/",
+  baseURL: "http://52.62.65.60:8080/api/v1/",
 });
 
 const token = localStorage.getItem("token");
